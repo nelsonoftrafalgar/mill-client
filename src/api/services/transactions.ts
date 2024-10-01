89ac9a309ae902b566e3ed500cc9023b8db90432
nelsonoftrafalgar <nelsonoftrafalgar@gmail.com>
2024-10-02 01:14:03 +0200

    update readme and add envs

--- src/api/services/transactions.ts
+++ src/api/services/transactions.ts
@@ -2,22 +2,24 @@ import { NewTransactionParams, Transaction } from '../types/transactions'
 
 import axios from 'axios'
 
+const apiUrl = import.meta.env.VITE_API_URL
+
 export const getTransactions = async (
 	page: number,
 	pageSize: number,
 	filterBeneficiary: string
 ) => {
 	const { data, headers } = await axios.get<Transaction[]>(
-		`http://localhost:3000/transactions?_page=${page}&_limit=${pageSize}&beneficiary_like=${filterBeneficiary}`
+		`${apiUrl}/transactions?_page=${page}&_limit=${pageSize}&beneficiary_like=${filterBeneficiary}`
 	)
 
 	return { transactions: data, headers }
 }
 
 export const createTransaction = async (payload: NewTransactionParams) => {
-	await axios.post('http://localhost:3000/transactions', payload)
+	await axios.post(`${apiUrl}/transactions`, payload)
 }
 
 export const deleteTransaction = async (id: number) => {
-	await axios.delete(`http://localhost:3000/transactions/${id}`)
+	await axios.delete(`${apiUrl}/transactions/${id}`)
 }
